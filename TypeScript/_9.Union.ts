c985cabfcffa245d33bb70e990f9f92cba25af98
Rohit Kumar <Mr.developer2003@gmail.com>
2026-02-15 16:36:50 +0530

    Union in Ts

---
+++ TypeScript/_9.Union.ts
@@ -0,0 +1,13 @@
+
+// Unions 
+
+let id:number | string;
+    id = 21;
+    id = 'm2132'
+
+    function printId(id:string | number) {
+        console.log(`id: ${id}`);
+    }
+
+    printId('xqw3132');
+    printId(4739);
